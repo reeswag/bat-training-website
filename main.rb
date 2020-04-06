1c64c6468a2016f7683c7ac0dad89fe3ca1a1d9d
reeswag <reeswag@gmail.com>
2020-04-06 14:16:14 +0100

    modular update

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'slim'
 require 'sass'
 require 'sinatra/flash'
@@ -7,24 +7,42 @@ require 'v8'
 require 'coffee-script'
 require './song'
 require './sinatra/auth'
+require './asset-handler'
 
-configure :development do
-    DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
-    set :email_address => 'not_a_real_email@gmail.com',
-        :email_user_name => 'not_a_real_user_name',
-        :email_password => 'not_a_real_password',
-        :email_domain => 'localhost.localdomain'
-end
+class Website < Sinatra::Base
+    use AssetHandler
+    register Sinatra::Auth
+    register Sinatra::Flash 
 
-configure :production do
-    DataMapper.setup(:default, ENV["DATABASE_URL"])
-    set :email_address => 'smtp.sendgrid.net',
-        :email_user_name => ENV['SENDGRID_USERNAME'],
-        :email_password => ENV['SENDGRID_PASSWORD'],
-        :email_domain => 'heroku.com'
-end
+    configure do
+        enable :sessions
+        set :username, 'frank'
+        set :password, 'sinatra'
+    end
 
-helpers do
+    configure :development do
+        set :email_address => 'not_a_real_email@gmail.com',
+            :email_user_name => 'not_a_real_user_name',
+            :email_password => 'not_a_real_password',
+            :email_domain => 'localhost.localdomain',
+            :start_time => Time.now 
+    end
+
+    configure :production do
+        set :email_address => 'smtp.sendgrid.net',
+            :email_user_name => ENV['SENDGRID_USERNAME'],
+            :email_password => ENV['SENDGRID_PASSWORD'],
+            :email_domain => 'heroku.com',
+            :start_time => Time.now
+    end
+
+    before do
+        last_modified settings.start_time
+        etag settings.start_time.to_s
+        cache_control :public, :must_revalidate  # makes the client confirm if there have been any changes to each web page since the start of the application before making a request. 
+        set_title # this assigns the title before loading each view
+    end
+
     def css(*stylesheets) # the * signifies that this function can take any number of arguments.
         stylesheets.map do |stylesheet|
             "<link href=\"/#{stylesheet}.css\" media=\"screen, projection\" rel =\"stylesheet\" />" # this generates
@@ -36,67 +54,61 @@ helpers do
     end
 
     def set_title
-        @title ||= "Bat Surveyor Training Resource"
+        @title ||= "Songs By Sinatra"
     end
 
     def send_message
         Pony.mail(
             :from => params[:name] + "<" + params[:email] + ">",
-            :to => 'reeswag@yahoo.co.uk',
+            :to => 'not_a_real_email@gmail.com',
             :subject => params[:name] + " has contacted you",
             :body => params[:message],
             :port => '587',
             :via => :smtp,
             :via_options => { 
-              :address              => 'smtp.gmail.com', 
-              :port                 => '587', 
-              :enable_starttls_auto => true, 
-              :user_name            => 'not_a_real_user_name', 
-              :password             => 'not_a_real_password', 
-              :authentication       => :plain, 
-              :domain               => 'localhost.localdomain'
+            :address              => 'smtp.gmail.com', 
+            :port                 => '587', 
+            :enable_starttls_auto => true, 
+            :user_name            => 'not_a_real_user_name', 
+            :password             => 'not_a_real_password', 
+            :authentication       => :plain, 
+            :domain               => 'localhost.localdomain'
             }
-          )
-        end
-end 
+        )
+    end 
 
-before do
-    set_title
-end
+    # get('/styles.css'){ scss :styles } # This employs the sass helper to tell Sinatra to process this request using Sass using the styles file located within the views directory. - moved to asset-handler.rb
+    # get('/javascripts/application.js'){ coffee :application } # this employs the coffee helper method to tell Sinatra to process the request using CoffeeScript using the application file in the views directory. - moved to asset-handler.rb
 
-DataMapper.finalize
+    get '/' do
+        slim :home
+    end
 
-get('/styles.css'){ scss :styles } # This employs the sass helper to tell Sinatra to process this request using Sass using the styles file located within the views directory.
-get('/javascripts/application.js'){ coffee :application } # this employs the coffee helper method to tell Sinatra to process the request using CoffeeScript using the application file in the views directory.
+    get '/about' do
+        @title = "All About This Website"
+        slim :about
+    end
 
-get '/' do
-    slim :home
-end
+    get '/contact' do
+        @title = "Contact Us"
+        slim :contact
+    end
 
-get '/about' do
-    @title = "All About This Website"
-    slim :about
-end
+    get '/set/:name' do
+        session[:name] = params[:name]
+    end
 
-get '/contact' do
-    @title = "Contact Us"
-    slim :contact
-end
+    get '/get/hello' do
+        "Hello #{session[:name]}"
+    end
 
-get '/set/:name' do
-    session[:name] = params[:name]
-end
+    not_found do
+        slim :not_found
+    end
 
-get '/get/hello' do
-    "Hello #{session[:name]}"
-end
-
-post '/contact' do
-    send_message
-    flash[:notice]="Thank you for your message. I'll be in touch soon."
-    redirect to('/')
-end
-
-not_found do
-    slim :not_found
+    post '/contact' do
+        send_message
+        flash[:notice]="Thank you for your message. We'll be in touch soon."
+        redirect to('/')
+    end
 end
