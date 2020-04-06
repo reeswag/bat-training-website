a2457c891744d1e300b280759fb59d384a09ca37
reeswag <reeswag@gmail.com>
2020-04-06 14:03:23 +0100

    mailer

--- main.rb
+++ main.rb
@@ -42,7 +42,7 @@ helpers do
     def send_message
         Pony.mail(
             :from => params[:name] + "<" + params[:email] + ">",
-            :to => 'not_a_real_email@gmail.com',
+            :to => 'reeswag@yahoo.co.uk',
             :subject => params[:name] + " has contacted you",
             :body => params[:message],
             :port => '587',
@@ -93,7 +93,7 @@ end
 
 post '/contact' do
     send_message
-    flash[:notice]="Thank you for your message. We'll be in touch soon."
+    flash[:notice]="Thank you for your message. I'll be in touch soon."
     redirect to('/')
 end
 
